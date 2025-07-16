ca5ccf579aff72699cc83d9431b770a53ea81969
ainame <s.namai.09@gmail.com>
2025-07-16 05:47:15 +0100

    Replace PathKit dependency with ainame fork
    Switched from official PathKit repository to ainame's fork, pinning to specific master branch commit 03ce551397e2cd85d3e137fd1914ee68e6b9a642.

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/tadija/AEXML.git", .upToNextMinor(from: "4.7.0")),
-        .package(url: "https://github.com/kylef/PathKit.git", .upToNextMinor(from: "1.0.1")),
+        .package(url: "https://github.com/ainame/PathKit.git", .revision("03ce551397e2cd85d3e137fd1914ee68e6b9a642")),
     ],
     targets: [
         .target(name: "XcodeProj",
